a952080c894c01d23e4b27b23d86f077817c424e
Exhen <exhen32@live.com>
2020-04-18 13:20:10 +0800

    Add files via upload

---
+++ 删除自带软件.bat
@@ -0,0 +1,30 @@
+% 放心删除 %
+adb shell pm uninstall --user 0 com.mitv.gallery
+adb shell pm uninstall --user 0 com.xiaomi.mitv.upgrade
+adb shell pm uninstall --user 0 com.xiaomi.mibox.lockscreen
+adb shell pm uninstall --user 0 com.xiaomi.mitv.advertise
+adb shell pm uninstall --user 0 com.xiaomi.voicecontrol
+adb shell pm uninstall --user 0 com.duokan.airkan.tvbox
+adb shell pm uninstall --user 0 com.xiaomi.mitv.tvpush.tvpushservice
+adb shell pm uninstall --user 0 com.xiaomi.devicereport
+adb shell pm uninstall --user 0 com.xiaomi.mimusic2
+adb shell pm uninstall --user 0 com.xiaomi.mitv.pay
+adb shell pm uninstall --user 0 com.xiaomi.mitv.payment
+adb shell pm uninstall --user 0 com.mipay.wallet.tv
+adb shell pm uninstall --user 0 com.xiaomi.account
+adb shell pm uninstall --user 0 com.xiaomi.account.auth
+adb shell pm uninstall --user 0 com.xiaomi.screenrecorder
+adb shell pm uninstall --user 0 com.xiaomi.statistic
+adb shell pm uninstall --user 0 com.miui.daemon
+adb shell pm uninstall --user 0 com.xiaomi.tv.nameservice
+adb shell pm uninstall --user 0 com.xiaomi.tweather
+adb shell pm uninstall --user 0 com.xiaomi.mitv.calendar
+adb shell pm uninstall --user 0 com.svox.pico
+adb shell pm uninstall --user 0 com.droidlogic
+% 谨慎删除 %
+% 搜狗输入法 %
+% adb shell pm uninstall --user 0 com.sohu.inputmethod.sogou.tv  % 
+% 桌面 %
+% adb shell pm uninstall --user 0 com.mitv.tvhome %
+% 屏保 %
+% adb shell pm uninstall --user 0 com.mitv.screensaver% 
